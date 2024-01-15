dff8f8ba18d6e9ac1dc8291936b28be41fb96308
alissonoliveira1 <alissonoliveira201339@gmail.com>
2024-01-15 18:15:47 -0300

    Cronometro feito com ReactJs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Style from './style.css'
+class App extends Component{
+constructor(props){
+  super(props)
+  this.state = {
+    numero: 0,
+    botao: 'Iniciar'
+  }
+  this.iniciar = "Iniciar"
+  this.tempo = null
+  this.vai = this.vai.bind(this)
+  this.limpar = this.limpar.bind(this)
+}
+vai(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let state = this.state
+if(this.tempo !== null){
+  clearInterval(this.tempo)
+  this.tempo = null
+  this.state.botao = 'Iniciar'
+  
+}
+else{
+this.tempo = setInterval(()=>{
+let state = this.state;
+state.numero += 0.1
+this.setState(state)
+},100)
+this.state.botao = 'pausa'
+}
+this.setState(state);
 }
 
-export default App;
+
+
+limpar(){
+clearInterval(this.tempo)
+let state = this.state;
+this.state.numero = 0
+this.setState(state)
+this.tempo = null
+this.state.botao = 'Iniciar'
+}
+
+
+  render(){
+    return(
+      <div className='container'>
+        <img className='img' src={require('./imagem/cronometro.png')}/>
+        <a className='tempo'>{this.state.numero.toFixed(1)}</a>
+        <div className='paibutton'>
+          <a className='iniciar' onClick={this.vai}>{this.state.botao}</a>
+          <a className='limpar' onClick={this.limpar}>Zerar</a>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default App
